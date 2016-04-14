1b20d8a5d72d49d853ec857e0d5c12d260af807e
Yaxiong Zhao <justicezyx@gmail.com>
2016-04-13 18:18:43 -0700

    Update.

--- postorder_iterative.cc
+++ postorder_iterative.cc
@@ -1,48 +1,71 @@
 #include "common/common.h"
-#include "node.h"
+
+#include <iostream>
+
+struct node {
+  node(int v) : data(v), left(nullptr), right(nullptr) {}
+
+  int data;
+  node* left;
+  node* right;
+};
+
+std::ostream& operator<<(std::ostream& os, const node& n) {
+  os << n.data << " ";
+  return os;
+}
 
 std::vector<int> postorder_iterative(node* root) {
-    std::vector<int> res;
-    if (root == NULL) {
-        return res;
+    std::vector<int> result;
+    if (root == nullptr) {
+        return result;
     }
 
-    node* prev = NULL;
+    node* prev = nullptr;
     std::stack<node*> h;
     h.push(root);
 
-    bool left_done = false;
     while (!h.empty()) {
-        std::cout<<"stack size:"<<h.size()<<std::endl;
-        node* cur = h.top();
-        if (cur->left == prev || cur->left == NULL) {
-            std::cout<<"left is done"<<std::endl;
-            left_done = true;
-            h.pop();
-            res.push_back(cur->data);
-            prev = cur;
-            if (cur->right != NULL) {
-                std::cout<<"push right of "<<cur->data<<std::endl;
-                h.push(cur->right);
-            }
-        } else if (left_done && (cur->right == prev || cur->right == NULL)) {
-            std::cout<<"right is done"<<std::endl;
-            res.push_back(cur->data);
-            h.pop();
-            prev = cur;
-        } else {
-            std::cout<<"push left of "<<cur->data<<std::endl;
-            h.push(cur->left);
+      node* curr = h.top();
+      if (prev == nullptr || prev->left == curr || prev->right == curr) {
+        if (curr->left) {
+          h.push(curr->left);
+        } else if (curr->right) {
+          h.push(curr->right);
         }
+      } else if (curr->left == prev) {
+        // Traversed left branch.
+        if (curr->right) {
+          h.push(curr->right);
+        }
+      } else {
+        // All sub-trees are traversed.
+        result.push_back(curr->data);
+        h.pop();
+      }
+      prev = curr;
     }
+    return result;
 }
 
 int main() {
-    node* root = new node(0);
-    node* left = new node(-1);
-    node* right = new node(1);
-    root->left = left;
-    left->left = right;
-    std::vector<int> res = postorder_iterative(root);
-    print(res.begin(), res.end());
+  //       0
+  //   -1      1
+  // -2  -3  5   2
+  auto* root = new node(0);
+  auto* left = new node(-1);
+  auto* right = new node(1);
+  auto* node_minus_2 = new node(-2);
+  auto* node_2 = new node(2);
+  auto* node_minus_3 = new node(-3);
+  auto* node_5 = new node(5);
+  root->left = left;
+  root->right = right;
+  left->left = node_minus_2;
+  left->right = node_minus_3;
+  right->left = node_5;
+  right->right = node_2;
+
+  std::vector<int> res = postorder_iterative(root);
+  puzzles::print(res.begin(), res.end());
 }
