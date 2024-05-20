60ce64e9c7c76c1e76ffd134cad9fed696057611
Yaxiong Zhao <justicezyx@gmail.com>
2024-05-20 12:28:28 -0700

    Add Allocate() skeleton
    Signed-off-by: Yaxiong Zhao <justicezyx@gmail.com>

--- puzzles-go/mempool/mempool.go
+++ puzzles-go/mempool/mempool.go
@@ -1,9 +1,26 @@
 package mempool
 
+type MemRegion struct {
+	offset int
+	size int
+}
+
 type MemPool struct {
+	// The actual memory
+	mem []byte
+}
+
+func (p MemPool) Cap() int {
+	return len(p.mem)
 }
 
 func NewMemPool(size int) (*MemPool, error) {
-	return nil, nil
+	res := MemPool {
+		mem: make([]byte, size),
+	}
+	return &res, nil
 }
 
+func (p *MemPool) Allocate(size int) (MemRegion, error) {
+	return MemRegion{}, nil
+}
--- puzzles-go/mempool/mempool_test.go
+++ puzzles-go/mempool/mempool_test.go
@@ -5,8 +5,16 @@ import (
 )
 
 func TestMemPool(t *testing.T) {
-	_, err := NewMemPool(100)
+	p, err := NewMemPool(100)
 	if err != nil {
 		t.Fatalf("Failed to create memory pool with size 100, error %v", err)
+	}
+	if p.Cap() != 100 {
+		t.Fatalf("Memory pool capacity should be 100, got %d", p.Cap())
+	}
+
+	_, err = p.Allocate(100)
+	if err != nil {
+		t.Fatalf("Failed to allocate memory with size 100, error %v", err)
 	}
 }
